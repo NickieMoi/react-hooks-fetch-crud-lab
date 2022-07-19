5a5d9b8549ff08204ff8519f612dc8309005815f
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-19 23:24:17 +0300

    commit tree 91bc0e21edae6a692bfad6919d042700a994ac48

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,12 @@
+
 import React from "react";
 
 function QuestionItem({ question }) {
+function QuestionItem({ question , onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
+  console.log(`Answers should ${question}`)
+
+
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,17 +14,21 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+
+
   return (
     <li>
       <h4>Question {id}</h4>
+    <li >
+      <h4>lorem testum {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
       <button>Delete Question</button>
+      <button onClick={() => onDelete(id) }>Delete Question</button>
     </li>
   );
 }
-
 export default QuestionItem;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
+import QuestionItem from "./QuestionItem.js";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  // const [isSett, setIsest] = useState(false)
+  // const [myId, setMyId] = useState('')
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(res => {
+      setQuestions(res);
+      // setIsest(true);
+    });
+  }, [])
+
+
+
+  // const toFilterDelete = questions.filter(que => {
+  //   if(myId === ''){
+  //     return true
+  //   } else {
+  //     return myId !== que.id
+  //   }
+  // })
+  function handleDeleteClick(id){
+    // setMyId(id)
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method : "DELETE"
+    })
+    .then(res => res.json())
+
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(res => {
+      setQuestions(res);
+      // setIsest(true);
+    });
+  }
+
+  const questionItem = questions.map(quest => (
+    <QuestionItem question = { quest}/>
+    <QuestionItem question = { quest } onDelete = { handleDeleteClick }/>
+  ))
+  console.log(questions)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionItem}
+      </ul>
     </section>
   );
 }
-
-export default QuestionList;
+export default QuestionList;W
